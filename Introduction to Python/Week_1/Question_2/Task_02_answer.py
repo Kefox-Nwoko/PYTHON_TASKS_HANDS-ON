2a5fe203aa49b853fda0f3fa8030bb8aded17ef5
Kefox-Nwoko <kefox.nwoko@gmail.com>
2023-09-15 00:08:07 +0100

    Completion of Task 2 solution

--- Introduction to Python/Week_1/Question_2/Task_02_answer.py
+++ Introduction to Python/Week_1/Question_2/Task_02_answer.py
@@ -1,3 +1,6 @@
-choice_numbers = input("Enter two numbers: ")
-12, 20 == choice_numbers.split(",")
-print(f'Sum of 12 and 20 = {int(12) + int(20)}')
+# Get user input for two numbers separated by space
+choice_numbers = input("Enter any two numbers: ")
+# num1, num2 = map(int, choice_numbers.split())
+num1, num2 = map(int, choice_numbers.split())
+sum_result = num1 + num2
+print(f'Sum of {num1} and {num2} = {sum_result}')
