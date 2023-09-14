62a12bd841a64079e787e0cffca2ff7f7b80a1f8
Kefox-Nwoko <kefox.nwoko@gmail.com>
2023-09-14 06:33:44 +0100

    Commit 2

--- Introduction to Python/Week_1/Question_1/Task_01_answer.py
+++ Introduction to Python/Week_1/Question_1/Task_01_answer.py
@@ -1,32 +1,38 @@
-# char01 = input("Enter a character: ")
-# print(f"You entered character: '{char01}'")
+char01 = input("Enter a character: ")
+print(f"You entered character: '{char01}'")
 
-# extra_char = input("Enter another character: ")
-# print(f"You entered unsigned character: '{extra_char}'")
+extra_char = input("Enter another character: ")
+print(f"You entered unsigned character: '{extra_char}'")
 
-# signed_short = input("Enter a signed short value: ")
-# print(f"You entered signed short: {signed_short}")
+signed_short = input("Enter a signed short value: ")
+print(f"You entered signed short: {signed_short}")
 
-# unsigned_short = input("Enter an unsigned short value: ")
-# print(f"You entered unsigned short: {unsigned_short}")
+unsigned_short = input("Enter an unsigned short value: ")
+print(f"You entered unsigned short: {unsigned_short}")
 
-# signed_integer = input("Enter a signed integer value: ")
-# print(f"You entered signed int: {signed_integer}")
+signed_integer = input("Enter a signed integer value: ")
+print(f"You entered signed int: {signed_integer}")
 
-# unsigned_integer = input("Enter an unsigned integer value: ")
-# print(f"You entered unsigned int: {unsigned_integer}")
+unsigned_integer = input("Enter an unsigned integer value: ")
+print(f"You entered unsigned int: {unsigned_integer}")
 
-# signed_long = input("Enter a signed long value: ")
-# print(f"You entered signed long: {signed_long}")
+signed_long = input("Enter a signed long value: ")
+print(f"You entered signed long: {signed_long}")
 
-# unsigned_long = input("Enter an unsigned long value: ")
-# print(f"You entered unsigned long: {unsigned_long}")
+unsigned_long = input("Enter an unsigned long value: ")
+print(f"You entered unsigned long: {unsigned_long}")
 
-# signed_long2 = input("Enter a signed long long value: ")
-# print(f"You entered signed long long: {signed_long2}")
+signed_long2 = input("Enter a signed long long value: ")
+print(f"You entered signed long long: {signed_long2}")
 
 unsigned_long2 = input("Enter an unsigned long long value: ")
 print(f"You entered unsigned long long: {unsigned_long2}")
 
+float_value = input("Enter a float value: ")
+print(f"You entered float: {float_value}")
 
+double_value = input("Enter a double value: ")
+print(f"You entered double: {double_value}")
 
+long_double_value = input("Enter a long double value: ")
+print(f"You entered long double: {long_double_value}")
