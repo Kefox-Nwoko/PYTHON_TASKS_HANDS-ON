a3fc2aadc7d6df8956feef9f6cac4505a7bf1b26
Kefox-Nwoko <kefox.nwoko@gmail.com>
2023-09-14 05:22:03 +0100

    First Commit

---
+++ Introduction to Python/Week_1/Question_1/Task_01_answer.py
@@ -0,0 +1,21 @@
+# char01 = input("Enter a character: ")
+# print(f"You entered a character: '{char01}'")
+
+# extra_char = input("Enter another character: ")
+# print(f"You entered another character: '{extra_char}'")
+
+# short_value_signed = input("Enter a signed short value: ")
+# print(f"You entered a signed short value: {short_value_signed}")
+
+# short_value_unsigned = input("Enter an unsigned short value: ")
+# print(f"You entered an unsigned short value: {short_value_unsigned}")
+
+# integer_value_signed = input("Enter an signed integer value: ")
+# print(f"You entered an signed integer value: {integer_value_signed}")
+
+# integer_value_unsigned = input("Enter an unsigned integer value: ")
+# print(f"You entered an signed integer value: {integer_value_unsigned}")
+
+
+
+
