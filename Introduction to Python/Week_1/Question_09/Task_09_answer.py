8936a08542dd9065ad04a8bdff09a4f26bf6ece3
Kefox-Nwoko <kefox.nwoko@gmail.com>
2023-09-15 17:13:05 +0100

    Commit task 10 answer

---
+++ Introduction to Python/Week_1/Question_09/Task_09_answer.py
@@ -0,0 +1,5 @@
+#user input to enter base
+base = int(input("Enter base: "))
+exponent = int(input("Enter exponent: "))
+output = int(base) ** int(exponent)     
+print(f'{base} ^ {exponent} = {output}')
---
+++ Introduction to Python/Week_1/Question_10/Task_10_answer.py
@@ -0,0 +1,5 @@
+import math
+#user input t
+user_input = int(input("Enter any number to find square root: "))
+square_root = math.sqrt(user_input)
+print(f'Square root of {user_input:.2f} = {square_root:.2f}')
